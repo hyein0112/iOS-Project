1e7b137627a5cdba96c9d690289d6552c0ca982a
hyein0112 <bhi12134@gmail.com>
2021-10-26 00:09:38 +0900

    good

---
+++ Programmatically/Spotify/Spotify/Controllers/Core/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchViewController.swift
+//  Spotify
+//
+//  Created by GSM02 on 2021/10/06.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+
+}
